4d37f0e2f224b8e828dbc52f5a974ec6f53b3c48
clach04 <clach04@gmail.com>
2025-04-10 20:36:28 -0700

    Display version in config page

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -1,3 +1,5 @@
+var meta = require('../../package.json');
+
 module.exports = [
     { 
         "type": "heading", 
@@ -107,5 +109,9 @@ module.exports = [
     {
         "type": "submit",
         "defaultValue": "Save"
+    },
+    {
+        "type": "text",
+        "defaultValue": "v" + meta.version
     }
 ];
